a8893fd15baf989bf584649e0b0d64ebf4b6bf44
BestiSuleymanova <127179716+bestisuleymanova@users.noreply.github.com>
2023-04-14 03:12:12 -0700

    Add files via upload

---
+++ o7.array-methods/script.js
@@ -0,0 +1,52 @@
+// 1.1
+// const arr1 = ['a', 'b', 'c', 'd', 'e'];
+// arr1.splice(1,1)
+// console.log(arr1);
+
+// 1.2
+// arr1.splice(2,2)
+// console.log(arr1);
+
+// 1.3
+// arr1.splice(arr1.length-1)
+// console.log(arr1);
+
+// 1.4
+// arr1.splice(arr1.length-3)
+// console.log(arr1);
+
+// 1.5
+// arr1.splice(3, 0, "3", "4");
+// console.log(arr1);
+
+//1.6 
+//arr1.slace(0,2,"A","B")
+// console.log(arr1);
+
+
+// /////.2.1 Slice method
+// const arr2 = ['user1', 'user2', 'user3', 11, 3, 5]
+// newArr=arr2.slice()
+// console.log(newArr);
+
+//2.2
+// const arr2 = ['user1', 'user2', 'user3', 11, 3, 5]
+// newArr=arr2.slice(1,5)
+// console.log(newArr);
+
+//2.3
+// const arr2 = ['user1', 'user2', 'user3', 11, 3, 5]
+// newArr=arr2.slice(2,6)
+// console.log(newArr);
+
+//3.1 concat method
+// const arr3 = [1, 2, 3]
+// const arr33 = [4, 5, 6]
+// const newArr = arr3.concat(arr33)
+// console.log(newArr);
+
+//3.2
+// const arr3 = [1, 2, 3]
+// const arr = ['a', 'b']
+// const newArr = arr3.concat(arr)
+// console.log(newArr);
